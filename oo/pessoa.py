3b4f912f7fd3af42e7c8abfc3b2f0ad536a042d7
Diego Ubirajara <diego.ubirajara@mail.com>
2018-04-14 10:17:20 +0200

    create instance attrs

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,8 +1,13 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=35):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
-        return 'Hello'
+        return f'Hello {self.nome}'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Diego')
     print(p.cumprimentar())
+    print(p.nome, p.idade)
