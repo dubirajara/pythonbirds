0e4e1a4dec71ad3da2f821e036423935daddbfbc
Diego Ubirajara <diego.ubirajara@mail.com>
2018-04-17 23:19:57 +0200

    create car class

--- oo/carro.py
+++ oo/carro.py
@@ -89,22 +89,36 @@ O   L
     'Oeste'
 """
 
-class Motor:
-    def __init__(self):
-        self.velocidade = 0
+
+class Carro:
+    def __init__(self, direcao, motor):
+        self.motor = motor
+        self.direcao = direcao
+
+    def calcular_velocidade(self):
+        return self.motor.velocidade
 
     def acelerar(self):
-        self.velocidade += 1
+        self.motor.acelerar()
 
     def frear(self):
-        self.velocidade -= 2
-        self.velocidade = max(0, self.velocidade)
+        self.motor.frear()
+
+    def calcular_direcao(self):
+        return self.direcao.valor
+
+    def girar_a_direita(self):
+        self.direcao.girar_a_direita()
+
+    def girar_a_esquerda(self):
+        self.direcao.girar_a_esquerda()
 
 
 NORTE = 'Norte'
 SUL = 'Sul'
 LESTE = 'Leste'
 OESTE = 'Oeste'
+
 
 class Direcao:
     rotacao_a_direita_dct = {
@@ -123,3 +137,14 @@ class Direcao:
     def girar_a_esquerda(self):
         self.valor = self.rotacao_a_esquerda_dct[self.valor]
 
+
+class Motor:
+    def __init__(self):
+        self.velocidade = 0
+
+    def acelerar(self):
+        self.velocidade += 1
+
+    def frear(self):
+        self.velocidade -= 2
+        self.velocidade = max(0, self.velocidade)
